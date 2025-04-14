0187437a7a6be51bacdea5a60ef7e36b484d2786
Fabiano Honorato <fabianoshz@gmail.com>
2025-04-14 17:56:46 -0300

    Use inputplumber fork

--- nixos/hosts/GipsyAvenger/configuration.nix
+++ nixos/hosts/GipsyAvenger/configuration.nix
@@ -98,11 +98,6 @@
     initrd.systemd.enable = true;
 
     kernelParams = ["quiet" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "hid_nintendo" "hid_playstation"];
-    kernelModules = [ "xpad" ];
-
-    extraModulePackages = [
-      (config.boot.kernelPackages.callPackage ../../../pkgs/xpad/xpad.nix {})
-    ];
   };
 
   system = {
--- nixos/optional/inputplumber/inputplumber.nix
+++ nixos/optional/inputplumber/inputplumber.nix
@@ -4,10 +4,16 @@
   nixpkgs.overlays = [
     (final: prev: {
       inputplumber = prev.inputplumber.overrideAttrs(old: {
-	postInstall = (old.postInstall or "") + ''
-	  cp -r ${./devices/40-flydigi-vader-4-pro.yaml} $out/share/inputplumber/devices/40-flydigi-vader-4-pro.yaml
-	  cp -r ${./capability_maps/flydigi-vader-4-pro.yaml} $out/share/inputplumber/capability_maps/flydigi-vader-4-pro.yaml
-	'';	
+        src = prev.fetchFromGitHub {
+          owner = "winghugs";
+          repo = "InputPlumber";
+          rev = "e76da54b5cbd8150fe56d676194d9bc90116ba1e";
+          hash = "sha256-s/eDu0wW3vOousB4fVwKyQVyiUSVchIKiMc8NkHECkM=";
+        };
+
+        checkFlags = [
+          "--skip=drivers::flydigi_vader_4_pro::hid_report_test::test_flydigi_vader_4_pro"
+        ];
       });
     })
   ];
