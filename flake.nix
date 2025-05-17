9787c3922eb0412e5417ff92765c3c3bd20b65c6
Fabiano Honorato <fabianoshz@gmail.com>
2025-05-17 09:22:30 -0300

    Flake update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
 
     home-manager = {
       # url = "github:nix-community/home-manager/release-25.05";
