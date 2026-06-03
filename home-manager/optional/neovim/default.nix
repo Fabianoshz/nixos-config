277c3bb73bbb45838d67ae829f5929f4369d8b15
Fabiano Honorato <fabianoshz@gmail.com>
2026-06-03 12:22:17 -0300

    Small things

--- home-manager/optional/neovim/default.nix
+++ home-manager/optional/neovim/default.nix
@@ -17,13 +17,13 @@
         plugin = nvim-treesitter;
         type = "lua";
         config = ''
-          require('nvim-treesitter.configs').setup({
-            highlight = {
-              enable = true,
-            },
-            indent = {
-              enable = true,
-            },
+          vim.api.nvim_create_autocmd('FileType', {
+            callback = function(args)
+              local ok = pcall(vim.treesitter.start, args.buf)
+              if ok then
+                vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+              end
+            end,
           })
         '';
       }
